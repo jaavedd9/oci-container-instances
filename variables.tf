728741fb73611d4da36114bf014caf7c5f87299d
Christophe Pruvost <christophe.pruvost@oracle.com>
2023-10-24 18:00:53 +0200

    update for oci stack resource manager

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 ### These information are needed only outside of OCI Terraform Stack Manager - Start
-variable "tenancy_ocid" {
-    description = "The OCI Tenancy ocid"
-    type        = string
-}
+# variable "tenancy_ocid" {
+#     description = "The OCI Tenancy ocid"
+#     type        = string
+# }
 
-variable "user_ocid" {
-    description = "The OCI User ocid"
-    type        = string
-}
+# variable "user_ocid" {
+#     description = "The OCI User ocid"
+#     type        = string
+# }
 
-variable "fingerprint" {
-    description = "The Fingerprint of the OCI API Key"
-    type        = string
-}
+# variable "fingerprint" {
+#     description = "The Fingerprint of the OCI API Key"
+#     type        = string
+# }
 
-variable "private_key_path" {
-    description = "The Path of the OCI API Key"
-    type        = string
-}
+# variable "private_key_path" {
+#     description = "The Path of the OCI API Key"
+#     type        = string
+# }
 ### These information are needed only outside of OCI Terraform Stack Manager - End
 
 variable "region" {
