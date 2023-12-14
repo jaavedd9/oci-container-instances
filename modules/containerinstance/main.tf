4c61d3b995c82396c21fcde2a13f3be6eeccf8bf
jaavedd9 <38977172+jaavedd9@users.noreply.github.com>
2023-12-14 14:04:40 +0300

    rename opensearch variable

--- modules/containerinstance/main.tf
+++ modules/containerinstance/main.tf
@@ -133,7 +133,7 @@ resource "oci_container_instances_container_instance" "this" {
         "AES_MASTER_KEY" = "${var.ci_container_env_variables.AES_MASTER_KEY}"
         "DEVELOPMENT_MODE" = "${var.ci_container_env_variables.DEVELOPMENT_MODE}"
         "OPENSEARCH_LOG_ENABLED"="${var.ci_container_env_variables.OPENSEARCH_LOG_ENABLED}"
-        "OPENSEARCH_LOG_URL"="${var.ci_container_env_variables.OPENSEARCH_LOG_URL}"
+        "OPENSEARCH_LOG_DB_URL"="${var.ci_container_env_variables.OPENSEARCH_LOG_DB_URL}"
         "OPENSEARCH_LOG_USERNAME"="${var.ci_container_env_variables.OPENSEARCH_LOG_USERNAME}"
         "OPENSEARCH_LOG_PASSWORD"="${var.ci_container_env_variables.OPENSEARCH_LOG_PASSWORD}"
         "OPENSEARCH_LOG_INDEX"="${var.ci_container_env_variables.OPENSEARCH_LOG_INDEX}"
@@ -141,6 +141,7 @@ resource "oci_container_instances_container_instance" "this" {
         "DJANGO_LOG_LEVEL"="${var.ci_container_env_variables.DJANGO_LOG_LEVEL}"
         "APP_ENV"="${var.ci_container_env_variables.APP_ENV}"
         "SENTRY_DSN"="${var.ci_container_env_variables.SENTRY_DSN}"
+        "OPENSEARCH_SEARCH_DB_URL"="${var.ci_container_env_variables.OPENSEARCH_SEARCH_DB_URL}"
 
     }
     image_url             = var.ci_image_url
